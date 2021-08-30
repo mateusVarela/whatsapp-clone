5978a3153fbcb40f75a0a5cfbed2842fd2c2342d
Mateus Varela <mateus.varela@fw7.com.br>
2021-08-30 17:23:27 -0300

    feat: added image intro light in home page

--- src/components/chatIntro/chatIntro.js
+++ src/components/chatIntro/chatIntro.js
@@ -4,7 +4,7 @@ import "./chatIntro.css"
 export default function chatInfo() {
   return (
     <div className="chat-intro">
-      <img src="" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Captura_de_tela_de_2021-08-30_17-16-51.png" alt="" />
       <h1>Mantenha seu celular conectado.</h1>
       <h2>O WhatsApp conecta ao seu telefone para sincronizar suas mensagens.</h2>
     </div>
