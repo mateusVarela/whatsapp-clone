356dc99d47c7d2cf7012482f1d8f7f4b2300bd18
Mateus Varela <mateus.varela@fw7.com.br>
2021-08-30 17:52:55 -0300

    feat: added style to message area

--- src/components/chatIntro/chatIntro.js
+++ src/components/chatIntro/chatIntro.js
@@ -5,8 +5,9 @@ export default function chatInfo() {
   return (
     <div className="chat-intro">
       <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Captura_de_tela_de_2021-08-30_17-16-51.png" alt="" />
-      <h1>Mantenha seu celular conectado.</h1>
+      <h1>Mantenha seu celular conectado</h1>
       <h2>O WhatsApp conecta ao seu telefone para sincronizar suas mensagens.</h2>
+      <h2>Para reduzir o uso de dados, conecte seu celular a uma rede WI-FI</h2>
     </div>
   )
 }
